839e343e7c6c4ae861233099cc4c1f1479eb214f
maixuanson2003 <maixuanson2789@gmail.com>
2025-03-13 08:43:47 +0700

    update

--- app/Console/Commands/BookingStatistical.php
+++ app/Console/Commands/BookingStatistical.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
-class BookingTK extends Command
+class BookingStatistical extends Command
 {
     /**
      * The name and signature of the console command.
